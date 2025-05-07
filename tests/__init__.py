c64a0ce3b4aca8897cbfb399e78a4c3b7f94a69e
Aris Christoforidis <christoforidis.aris@gmail.com>
2025-05-07 18:07:16 +0300

    insert sys path on tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 import os
+import sys
 
 _TEST_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.abspath(os.path.join(_TEST_ROOT, "..", "src"))
-# if _PROJECT_ROOT not in sys.path: sys.path.insert(0, _PROJECT_ROOT)
+_PROJECT_ROOT = os.path.abspath(os.path.join(_TEST_ROOT, "..", "src", "sharp_star"))
+if _PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, _PROJECT_ROOT)
 _PATH_DATA = os.path.abspath(os.path.join(_TEST_ROOT, "..", "data", "splits"))
