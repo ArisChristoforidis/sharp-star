a8ffac4422909e2448b4ab8fdcd25b727d66a687
Aris Christoforidis <christoforidis.aris@gmail.com>
2025-05-09 20:53:49 +0300

    torch compile the eval

--- src/sharp_star/evaluate.py
+++ src/sharp_star/evaluate.py
@@ -49,6 +49,7 @@ def evaluate(
     eval_set = make_dataset(eval_path, mean, std)
 
     model.eval()
+    model = torch.compile(model)
     model.to(DEVICE)
 
     psnr = PeakSignalNoiseRatio().to(DEVICE)
